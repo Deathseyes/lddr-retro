92878035f588c3c1e0791a53f254486f14e8e16c
nijisou <375153450@qq.com>
2019-03-07 10:40:10 +0800

    add: 随机生成note

---
+++ Assets/Script/NoteSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawn : MonoBehaviour
+{
+    public Note note;
+    public float noteRate = 1;
+
+    private float nextNote = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextNote)
+        {
+            nextNote += noteRate;
+
+            Note clone = Instantiate(note);
+            clone.xSpeed = 0.85f * (int)(Random.Range(-1, 2));
+        } 
+    }
+}
